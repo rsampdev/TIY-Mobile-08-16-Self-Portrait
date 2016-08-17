8169390fd1434159d6ba6531c04d572b00f3c0ea
rsampdev <rsampdev@gmail.com>
2016-08-16 20:45:26 -0400

    Initial Commit

---
+++ Self Portrait/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Self Portrait
+//
+//  Created by Rodney Sampson on 8/16/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
